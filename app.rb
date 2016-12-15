28fe2bb8cb71de78e3a44d46711fd95ab59706cf
Bill Klinkow <klinkow@gmail.com>
2016-12-14 20:20:35 -0800

    develop front-end to allow user to create drinks objects and ingredient objects seperately

--- app.rb
+++ app.rb
@@ -4,12 +4,18 @@ require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
-
 get('/') do
   erb(:index)
 end
 
 get('/drinks') do
+  @drinks = Drink.all()
+  erb(:drinks)
+end
+
+delete('/drinks') do
+  @drink = Drink.find(params.fetch('drink_id'))
+  @drink.delete()
   @drinks = Drink.all()
   erb(:drinks)
 end
@@ -20,3 +26,31 @@ post('/drinks/:id') do
   @drinks = Drink.all()
   erb(:drinks)
 end
+
+patch('/drinks/:id') do
+  @drink = Drink.find(params.fetch('id'))
+  @drink.update({:name => params.fetch('new_drink_name'), :instructions => params.fetch('new_drink_instructions'), :description => params.fetch('new_drink_description')})
+  @drink.save()
+  @drinks = Drink.all()
+  erb(:drinks)
+end
+
+get('/drink/:id') do
+  @drink = Drink.find(params.fetch('id'))
+  erb(:drink)
+end
+
+get('/ingredients') do
+  @ingredients = Ingredient.all()
+  erb(:ingredients)
+end
+
+post('/ingredients/:id') do
+  @ingredient = Ingredient.new({:name => params.fetch('new_ingredient_name'), :description => params.fetch('new_ingredient_description')})
+  @ingredient.save()
+  @ingredients = Ingredient.all()
+  erb(:ingredients)
+end
+
+
+  # Quantity.find_by drink_id: test_drink.id(), ingredient_id: )
