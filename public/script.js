125b5ed35e8f2a556b9635b7bb1ac359e1ea7305
Bill Klinkow <klinkow@gmail.com>
2016-12-14 20:39:56 -0800

    minor changes to front end

---
+++ public/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+  $('select').material_select();
+
+
+
+  });
+
+  // $('#new_drink_ingredient').click(function(event) {
+  //   event.preventDefault();
+  //   $('#add_another_ingredient').append("<p>hello</p>");
