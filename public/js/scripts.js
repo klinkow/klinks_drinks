d54a8407e767b98c74d858e83241580f0006b534
Bill Klinkow <klinkow@gmail.com>
2017-01-08 20:43:07 -0800

    add search bar to top of drinks and ingredients page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-  $('.collapsible').collapsible();
   $('select').material_select();
-
-
+  $(".button-collapse").sideNav();
+  $('.collapsible').collapsible();
 
   });
 
